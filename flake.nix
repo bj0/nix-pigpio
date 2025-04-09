aa561c1fa07e3bc5df36ff535b658ac2423d46b7
Brian Parma <execrable@gmail.com>
2025-04-09 12:16:05 -0700

    add overlay

--- flake.nix
+++ flake.nix
@@ -7,19 +7,46 @@
     self,
     nixpkgs,
     utils,
-  }:
+  }: let
+    build = {
+      buildPythonPackage,
+      fetchPypi,
+    }:
+      buildPythonPackage rec {
+        pname = "pigpio";
+        version = "1.78";
+        src = fetchPypi {
+          inherit pname version;
+          hash = "sha256-ke+lDkmQZJ2pdAijhHgtbM9YNC/FnN/iHtekKRFWmXU=";
+        };
+      };
+  in
     utils.lib.eachDefaultSystem (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
       in {
-        packages.default = pkgs.python3Packages.buildPythonPackage rec {
-          pname = "pigpio";
-          version = "1.78";
-          src = pkgs.fetchPypi {
-            inherit pname version;
-            hash = "sha256-ke+lDkmQZJ2pdAijhHgtbM9YNC/FnN/iHtekKRFWmXU=";
-          };
+        # system specific python3 package
+        packages.default = build {
+          inherit (pkgs.python3Packages) buildPythonPackage;
+          inherit (pkgs) fetchPypi;
         };
       }
-    );
+    )
+    // {
+      # system independent overlay
+      overlays.default = _: prev: {
+        pythonPackagesExtensions =
+          prev.pythonPackagesExtensions
+          ++ [
+            (
+              _: pyprev: {
+                pigpio = build {
+                  inherit (pyprev) buildPythonPackage;
+                  inherit (prev) fetchPypi;
+                };
+              }
+            )
+          ];
+      };
+    };
 }
